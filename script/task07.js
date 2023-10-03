6fc2bcd090193b988802976e59b05d7a7e81c7e6
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-03 16:54:58 +0200

    task08

--- script/task07.js
+++ script/task07.js
@@ -1,8 +1,29 @@
 const footer = document.querySelector("footer div a");
 
+function setCookie(cookie_name, cookie_value, exdays) {
+  const date = new Date();
+  date.setTime(date.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expiration = "expires=" + date.toUTCString();
+  document.cookie = cookie_name + "=" + cookie_value + ";" + expiration + ";path=/";
+}
+
 footer.addEventListener("click", () => {
-  document.cookie = "acceptsCookies = true; max-age = 86400";
-  console.log(document.cookie);
+  setCookie("acceptsCookies", true, 1);
 });
 
+function displayMessage() {
+  const cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[1].split("=");
+    if (cokkie[0] === "acceptsCookies" && cookie[1] === "true") {
+      return false;
+    }
+  }
+    return true; 
+    }
 
+footer.addEventListener("click", () => {
+  if (displayMessage()) {
+    setCookie("acceptsCookies", true, 1);
+  }
+});
