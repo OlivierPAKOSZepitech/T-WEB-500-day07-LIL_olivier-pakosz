b0d8455bc9b63247a6c68a6410c67888790bbc53
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-04 17:07:39 +0200

    correction task11

--- task11/blue.js
+++ task11/blue.js
@@ -1,16 +1,17 @@
 const ps = document.querySelectorAll("p");
 
-const addClass = () => ps.forEach((p) => {
-  p.addEventListener("click", () => {
-    p.classList.toggle("highlighted");
-  });
-  p.addEventListener("mouseover", () => {
-    p.classList.add('blue');
+const addClass = () =>
+  ps.forEach((p) => {
+    p.addEventListener("click", () => {
+      p.classList.toggle("highlighted");
+    });
+    p.addEventListener("mouseover", () => {
+      p.classList.add("blue");
+    });
+
+    p.addEventListener("mouseout", () => {
+      p.classList.remove("blue");
+    });
   });
 
-  p.addEventListener("mouseout", () => {
-    p.classList.remove('blue');
-  });
-});
-
-addClass()
+addClass();
